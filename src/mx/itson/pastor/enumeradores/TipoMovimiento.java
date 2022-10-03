d953593d7a5730757b54a3d56bb48003a82ab61d
A20021 <aldrich.iberri228878@potros.itson.edu.mx>
2022-10-03 08:32:28 -0700

    Agrege nuevas clases

---
+++ src/mx/itson/pastor/enumeradores/TipoMovimiento.java
@@ -0,0 +1,13 @@
+
+package mx.itson.pastor.enumeradores;
+
+/**
+ *
+ * @author Aldrich Edsel Iberri Araiza
+ */
+public enum TipoMovimiento {
+    
+    CARGO,
+    ABONO
+    
+}
--- src/mx/itson/pastor/negocio/Movimiento.java
+++ src/mx/itson/pastor/negocio/Movimiento.java
@@ -2,6 +2,7 @@
 package mx.itson.pastor.negocio;
 
 import java.sql.Date;
+import mx.itson.pastor.enumeradores.TipoMovimiento;
 
 /**
  *
@@ -12,5 +13,6 @@ public class Movimiento {
     private String descripcion;
     private Date fecha;
     private double importe;
+    private TipoMovimiento tipo;
     
 }

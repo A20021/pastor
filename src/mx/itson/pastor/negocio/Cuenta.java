75090d07e1c612e1bf501f20739a2e135717ea3a
A20021 <aldrich.iberri228878@potros.itson.edu.mx>
2022-10-03 08:24:58 -0700

    Agrege nuevas clases

---
+++ src/mx/itson/pastor/negocio/Cuenta.java
@@ -0,0 +1,12 @@
+
+package mx.itson.pastor.negocio;
+
+/**
+ *
+ * @author Aldrich Edsel Iberri Araiza
+ */
+public class Cuenta {
+    
+    private ClienteNegocio cliente;
+    
+}
---
+++ src/mx/itson/pastor/negocio/Movimiento.java
@@ -0,0 +1,16 @@
+
+package mx.itson.pastor.negocio;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Aldrich Edsel Iberri Araiza
+ */
+public class Movimiento {
+    
+    private String descripcion;
+    private Date fecha;
+    private double importe;
+    
+}
